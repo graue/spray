c2600aed19a11700811b5f8cc89df9d3faaffcba
Catatonic Porpoise <graue@oceanbase.org>
2006-01-14 22:14:48 -0500

    Merge time.c and random.c into utils.c.
    darcs-hash:20060115031448-4b1db-81b13a870919cb662619256c771974f4bd8122bc

---
+++ utils.c
@@ -0,0 +1,73 @@
+#include "spray.h"
+#include "mt.h"
+#include <time.h>
+
+/*
+ * Random number routines.
+ */
+
+#if defined(__OpenBSD__) && !defined(PORTABLE)
+#define USE_RANDOM
+#endif
+
+void random_seed(void)
+{
+#ifdef USE_RANDOM
+	srandomdev();
+#else
+	init_genrand((unsigned long) time(NULL));
+#endif
+}
+
+int rnd3(int range)
+{
+	int r1, r2, r3;
+	r1 = rnd(range);
+	r2 = rnd(range);
+	r3 = rnd(range);
+	return (r1+r2+r3) / 3;
+}
+
+int rnd(int range)
+{
+#ifdef USE_RANDOM
+	return random() % range;
+#else
+	return genrand_int31() % range;
+#endif
+}
+
+/*
+ * Timer routines.
+ */
+
+static Uint32 next_time = 0;
+
+void timer_start(void)
+{
+	next_time = SDL_GetTicks() + TICK_INTERVAL;
+}
+
+static Uint32 time_left(void)
+{
+	Uint32 now;
+	now = SDL_GetTicks();
+	if (next_time <= now)
+		return 0;
+	else
+		return next_time - now;
+}
+
+void time_wait(void)
+{
+	Uint32 left;
+	left = time_left();
+	if (left > 10)
+	{
+		SDL_Delay(left);
+		left = time_left();
+	}
+	if (left > 0)
+		while (time_left()) { }
+	timer_start();
+}
