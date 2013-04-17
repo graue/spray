b75cf4e142b6a1e5b0305555609779b28c8c1644
Scott Feeney <scott@oceanbase.org>
2013-04-17 17:36:10 -0400

    Add joystick support
    Patch dated July 14th, 2010

--- input.c
+++ input.c
@@ -1,11 +1,41 @@
 #include "spray.h"
 
 static int keys = 0;
+static SDL_Joystick *joy = NULL;
+static int numaxes;
+static int numbuttons;
+
+/* initialize joystick if available */
+static void initinput(void)
+{
+	if (SDL_InitSubSystem(SDL_INIT_JOYSTICK) < 0)
+		return;
+
+	joy = SDL_JoystickOpen(0);
+	if (joy == NULL)
+		return;
+	numaxes = SDL_JoystickNumAxes(joy);
+	numbuttons = SDL_JoystickNumButtons(joy);
+	if (numaxes < 2 || numbuttons < 1)
+	{
+		SDL_JoystickClose(joy);
+		joy = NULL;
+	}
+}
 
 int getinput(void)
 {
 	SDL_Event event;
 	int pause = 0;
+	static int inited = 0;
+	static int lastbuttonstate = 0;
+	int buttonstate = 0;
+
+	if (!inited)
+	{
+		initinput();
+		inited = 1;
+	}
 
 	while (SDL_PollEvent(&event))
 	{
@@ -41,6 +71,36 @@ int getinput(void)
 		}
 	}
 
+	if (joy)
+	{
+		SDL_JoystickUpdate();
+		if (SDL_JoystickGetAxis(joy, 0) < 0)
+		{
+			keys &= ~KEY_RIGHT;
+			keys |= KEY_LEFT;
+		}
+		else if (SDL_JoystickGetAxis(joy, 0) > 0)
+		{
+			keys &= ~KEY_LEFT;
+			keys |= KEY_RIGHT;
+		}
+		if (SDL_JoystickGetAxis(joy, 1) < 0)
+		{
+			keys &= ~KEY_DOWN;
+			keys |= KEY_UP;
+		}
+		else if (SDL_JoystickGetAxis(joy, 1) > 0)
+		{
+			keys &= ~KEY_UP;
+			keys |= KEY_DOWN;
+		}
+
+		lastbuttonstate = buttonstate;
+		buttonstate = SDL_JoystickGetButton(joy, 0);
+		if (buttonstate && !lastbuttonstate)
+			pause = 1;
+	}
+
 	if (pause)
 		displaytext("PAUSED");
 
@@ -62,6 +122,11 @@ int getinput(void)
 				pause = 0;
 			}
 		}
+
+		lastbuttonstate = buttonstate;
+		buttonstate = SDL_JoystickGetButton(joy, 0);
+		if (buttonstate && !lastbuttonstate)
+			pause = 0;
 
 		if (!pause)
 			restorescreen();
